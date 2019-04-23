53fa51d4b0dd4a6c2aa9a02fe3024dba8aa22b57
nrbozzelli <nrbozzelli@gmail.com>
2019-04-23 15:35:00 -0600

    My age in dog years project

---
+++ app.js
@@ -0,0 +1,19 @@
+/* My age in dog years*/
+
+// my current age
+let myAge = 29;
+
+// the first 2 years of a dog's life
+    earlyYears = 21
+
+// calculation for the later years of a dog's
+    laterYears = (myAge - 2) * 4;
+
+// this is the forumla for my age in dog years
+    myAgeInDogYears = earlyYears + laterYears;
+
+// the variable for my name
+    myName = 'Nelson'.toLowerCase();
+
+// the final output from my code
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
